0c81ff0694b8a1f22d5f46840eebb1be6ca5b371
wandsu <wandsu@github.com>
2020-02-01 20:43:59 -0300

    Adding other calculator features

--- RestWithASP-Net/RestWithASP-Net/Controllers/CalculatorController.cs
+++ RestWithASP-Net/RestWithASP-Net/Controllers/CalculatorController.cs
@@ -9,12 +9,13 @@ namespace RestWithASP_Net.Controllers
     [Route("api/[controller]")]
     public class CalculatorController : Controller
     {
+        Verification verf = new Verification();
 
-        // GET api/values/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        // Sum
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
-            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            if(verf.IsNumeric(firstNumber) && verf.IsNumeric(secondNumber))
             {
                 var sum = Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber);
                 return Ok(sum.ToString());
@@ -22,17 +23,68 @@ namespace RestWithASP_Net.Controllers
             return BadRequest("Fail");
         }
 
-        private bool IsNumeric(string number)
+
+        // Substraction
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
         {
-            try
+            if (verf.IsNumeric(firstNumber) && verf.IsNumeric(secondNumber))
             {
-                Convert.ToDecimal(number);
-                return true;
+                var sum = Convert.ToDecimal(firstNumber) - Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
             }
-            catch
+            return BadRequest("Fail");
+        }
+
+
+        // Multiplication
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (verf.IsNumeric(firstNumber) && verf.IsNumeric(secondNumber))
             {
-                return false;
+                var sum = Convert.ToDecimal(firstNumber) * Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
             }
+            return BadRequest("Fail");
+        }
+
+
+        // Division
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (verf.IsNumeric(firstNumber) && verf.IsNumeric(secondNumber))
+            {
+                var sum = Convert.ToDecimal(firstNumber) / Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Fail");
+        }
+
+
+        // Exponentiation
+        [HttpGet("exponentiation/{firstNumber}/{secondNumber}")]
+        public IActionResult Exponentiation(string firstNumber, string secondNumber)
+        {
+            if (verf.IsNumeric(firstNumber) && verf.IsNumeric(secondNumber))
+            {
+                var sum = Math.Pow(Convert.ToDouble(firstNumber), Convert.ToDouble(secondNumber));
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Fail");
+        }
+
+        // Exponentiation
+        [HttpGet("squareroot/{firstNumber}")]
+        public IActionResult SquareRoot(string firstNumber, string secondNumber)
+        {
+            if (verf.IsNumeric(firstNumber) && verf.IsNumeric(secondNumber))
+            {
+                var sum = Math.Sqrt(Convert.ToDouble(firstNumber));
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Fail");
         }
     }
 }
---
+++ RestWithASP-Net/RestWithASP-Net/Verification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_Net
+{
+    public class Verification
+    {
+        public bool IsNumeric(string number)
+        {
+            try
+            {
+                Convert.ToDecimal(number);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
